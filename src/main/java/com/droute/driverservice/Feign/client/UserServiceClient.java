0a98acd77ea69eff8689054e65b9406619e22119
Anukrity Srivastava <anukritysrivastava@gmail.com>
2025-05-06 01:33:23 +0530

    Added reset-password, get-driver

--- src/main/java/com/droute/driverservice/Feign/client/UserServiceClient.java
+++ src/main/java/com/droute/driverservice/Feign/client/UserServiceClient.java
@@ -1,16 +1,17 @@
 package com.droute.driverservice.Feign.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.droute.driverservice.dto.UserEntity;
 import com.droute.driverservice.dto.request.LoginUserRequestDto;
 import com.droute.driverservice.dto.request.RegisterUserRequestDto;
+import com.droute.driverservice.dto.request.ResetPasswordRequestDTO;
 import com.droute.driverservice.dto.response.CommonResponseDto;
 
 @Component
@@ -18,13 +19,21 @@ import com.droute.driverservice.dto.response.CommonResponseDto;
 // @FeignClient(name = "droute-user-service", path = "/api/user")
 public interface UserServiceClient {
 
+   
     @PostMapping("/auth/login")
     CommonResponseDto<UserEntity> loginUser(@RequestBody LoginUserRequestDto loginRequest);
+
+    @GetMapping("/{email}")
+    CommonResponseDto<UserEntity> getUserByEmail(@PathVariable("email") String email);
 
     @PostMapping("/")
     CommonResponseDto<UserEntity> registerUser(@RequestBody RegisterUserRequestDto registerRequest);
 
-    @GetMapping("/{userId}")
-    public CommonResponseDto<UserEntity> getUserById(@PathVariable Long userId);
+    @GetMapping("/id/{userId}")
+    CommonResponseDto<UserEntity> getUserById(@PathVariable("userId") Long userId);
 
+    @PutMapping("/auth/reset-password")
+    CommonResponseDto<String> resetUserPassword(@RequestBody ResetPasswordRequestDTO requestDTO);
+
+   
 }
